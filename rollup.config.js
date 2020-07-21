d8e321ffe4628cbc51946e0cd2523fe77b5e90da
Brandon Ros <brandonros1@gmail.com>
2020-07-20 22:04:38 -0400

    rollup throwOnError false

--- rollup.config.js
+++ rollup.config.js
@@ -31,7 +31,7 @@ export default {
             ]
         }),
         tslint({
-            throwOnError: true
+            throwOnError: false
         }),
         builtins(),
         typescript({
